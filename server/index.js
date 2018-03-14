a23e784c1ec06473702caa1699ef586d5c6729e0
John Son <jsson77@gmail.com>
2018-03-13 19:19:19 -0700

    setup react app boilerplate

--- server/index.js
+++ server/index.js
@@ -1,35 +1,66 @@
-const express = require('express')
-const cors = require('cors')
-var bodyParser = require('body-parser')
+/* eslint consistent-return:0 */
 
-const utils = require('./utils')
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const app = express()
+const utils = require('./utils');
+const logger = require('./logger');
 
-app.use(bodyParser.json())
+const argv = require('./argv');
+const port = require('./port');
+const setup = require('./middlewares/frontendMiddleware');
+const isDev = process.env.NODE_ENV !== 'production';
+const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
+const resolve = require('path').resolve;
+const app = express();
 
-app.use(cors())
+app.use(bodyParser.json());
+
+app.use(cors());
 
 app.post('/items/:id', (req, res) => {
-  let rating = req.body.rating
-  let id = req.params.id
-  let resp = utils.write(id, rating)
-  if (resp) return res.send(resp)
-  return res.status(500).send('could not write result')
-})
+  const rating = req.body.rating;
+  const id = req.params.id;
+  const resp = utils.write(id, rating);
+  if (resp) return res.send(resp);
+  return res.status(500).send('could not write result');
+});
 
 app.get('/items', (req, res) => {
-  let amt = parseInt(req.query.amt) || 20
-  let page = parseInt(req.query.page) || 1
-  let resp = utils.read(amt, page)
-  if (resp) return res.send(resp)
-  return res.status(500).send('could not read results')
-})
+  const amt = parseInt(req.query.amt, 10) || 20;
+  const page = parseInt(req.query.page, 10) || 1;
+  const resp = utils.read(amt, page);
+  if (resp) return res.send(resp);
+  return res.status(500).send('could not read results');
+});
 
-app.listen('3000', (err) => {
+// In production we need to pass these values in instead of relying on webpack
+setup(app, {
+  outputPath: resolve(process.cwd(), 'build'),
+  publicPath: '/',
+});
+
+// get the intended host and port number, use localhost and port 3000 if not provided
+const customHost = argv.host || process.env.HOST;
+const host = customHost || null; // Let http.Server use its default IPv6/4 host
+const prettyHost = customHost || 'localhost';
+
+app.listen(port, host, (err) => {
   if (err) {
-    console.log('there was a problem starting the server')
-  } else {
-    console.log('server listening port 3000')
+    return logger.error(err.message);
   }
-})
+
+  // Connect to ngrok in dev mode
+  if (ngrok) {
+    ngrok.connect(port, (innerErr, url) => {
+      if (innerErr) {
+        return logger.error(innerErr);
+      }
+
+      logger.appStarted(port, prettyHost, url);
+    });
+  } else {
+    logger.appStarted(port, prettyHost);
+  }
+});
