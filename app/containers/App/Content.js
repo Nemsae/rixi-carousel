57f31141fd8163b8f68106c0256ccab43f03b6aa
John Son <jsson77@gmail.com>
2018-03-16 15:47:34 -0700

    update reade me

--- app/containers/App/Content.js
+++ app/containers/App/Content.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Content = styled.main`
   grid-area: content;
   overflow: auto;
-  ${'' /* overflow-x: auto; */}
+  ${''/* overflow-x: auto; */}
 `;
 
 export default Content;
--- app/containers/HomePage/Wrapper.js
+++ app/containers/HomePage/Wrapper.js
@@ -6,8 +6,8 @@ const Wrapper = styled.div`
 
   display: flex;
   flex-direction: column;
-  ${'' /* align-items: center; */}
-  ${'' /* justify-content: center; */}
+  ${''/* align-items: center; */}
+  ${''/* justify-content: center; */}
 
   overflow: auto;
 
