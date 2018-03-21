c804b97fbe82b666cd96faba23305fd0fcd72433
John Son <jsson77@gmail.com>
2018-03-20 21:07:46 -0700

    modularize breadcrumbs, put into component for resuability

--- README.md
+++ README.md
@@ -45,3 +45,16 @@
 ## Project Milestones:
 1. 100% Test Coverage
 2. Build script to scrape `//  TODO:` comments and build list into README.md  `(app/container/HomePage) => "Build case to render error component" `
+
+## TODOS:
+
+> Update dependencies, config files, and maybe JavaScript files that require babel packages directly
+
+- [x] Works on Node >= 4 (anything lower isn't supported in v7) ([#16](https://github.com/babel/babel-upgrade/pull/16))
+- [x] Run npm/yarn after updating dependencies (use `--install`) ([#18](https://github.com/babel/babel-upgrade/pull/18))
+- [x] Update `package.json`: `dependencies` and `devDependencies` to the "latest supported" version.
+  - [x] all package renames
+    - [x] `babel-loader` for webpack >=1 ([#34](https://github.com/babel/babel-upgrade/pull/34))
+    - [x] `rollup-plugin-babel` ([#36](https://github.com/babel/babel-upgrade/pull/36))
+  - [x] Upgrading the same package to the latest version
+  - [x] add `@babel/core` peerDep ([7c34cd](https://github.com/babel/babel-upgrade/commit/7c34cdf318ecbb8a916e7a8ee5c2cfbad7d8d8d0))
--- app/components/RixiCarousel/index.js
+++ app/components/RixiCarousel/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 // import { FormattedMessage } from 'react-intl';
 
 import MaterialIcon from 'components/MaterialIcon';
+import BreadCrumbs from 'components/BreadCrumbs';
 import CarouselBottom from './CarouselBottom';
 import CarouselTop from './CarouselTop';
 import CardImg from './CardImg';
@@ -12,7 +13,6 @@ import CardTop from './CardTop';
 import CarouselCards from './CarouselCards';
 import CarouselBreadCrumbs from './CarouselBreadCrumbs';
 import CarouselUX from './CarouselUX';
-import CircleLink from './CircleLink';
 import H2 from './H2';
 import H3 from './H3';
 import Wrapper from './Wrapper';
@@ -117,10 +117,8 @@ class RixiCarousel extends React.PureComponent {
     return (
       <Wrapper>
         <CarouselBreadCrumbs>
-          { this.renderBreadCrumbs() }
+          <BreadCrumbs total={this.state.total} page={this.state.page} changePage={this.changePage} />
         </CarouselBreadCrumbs>
-
-        {/* ------ Carousel Cards ------ */}
         <CarouselCards>
           <CarouselTop>
             <CarouselUX>
@@ -133,7 +131,6 @@ class RixiCarousel extends React.PureComponent {
             { CardBottoms }
           </CarouselBottom>
         </CarouselCards>
-
       </Wrapper>
     );
   }
